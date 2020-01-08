e515b2140102f936e945c4b967f7f51968d3961a
mairasala <mairasala@gmail.com>
2020-01-08 18:12:45 -0300

    change relations + return post with complete comment

--- api/post/controllers/Post.js
+++ api/post/controllers/Post.js
@@ -1,8 +1,22 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils')
+
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/guides/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.post.search(ctx.query);
+    } else {
+      entities = await strapi.services.post.find(ctx.query, ['contentcreator', 'comments.contentcreator', 'content', 'media', 'categories']);
+    }
+    const model = strapi.models.post
+
+    return entities.map(entity => sanitizeEntity(entity, { model }));
+  },
+};
