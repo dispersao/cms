b511cdf4bb2474675a448b825e20f63be23181b3
mairasala <mairasala@gmail.com>
2020-03-28 13:43:38 +0100

    store and respond properly appusers

--- api/appuser/controllers/Appuser.js
+++ api/appuser/controllers/Appuser.js
@@ -5,4 +5,48 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+  async create(ctx) {
+    let entity = await strapi.services.appuser.create(ctx.request.body)
+    console.log(entity)
+    return strapi.services.appuser.formatAppuser(entity)
+  },
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.appuser.search(ctx.query);
+    } else {
+      entities = await strapi.services.appuser.find(ctx.query);
+    }
+    return entities.map(entity => strapi.services.appuser.formatAppuser(entity))
+  },
+
+  async findOne(ctx) {
+    let entity = await strapi.services.appuser.findOne(ctx.params);
+    return strapi.services.appuser.formatAppuser(entity)
+  },
+
+  async update(ctx) {
+    let reqBody = ctx.request.body
+    if(ctx.request.body.script) {
+      let script = await strapi.services.script.findOne({token: ctx.request.body.script})
+      if (!script) {
+        return ctx.badRequest('Invalid session token.')
+      } else if(script.state === 'idle' || script.state === 'finished') {
+        return ctx.badRequest('This session is not available.')
+      } else {
+        reqBody = {
+          ...reqBody,
+          script: script.id
+        }
+      }
+    }
+    let entity = await strapi.services.appuser.update(
+      ctx.params,
+      reqBody
+    )
+    return strapi.services.appuser.formatAppuser(entity)
+  }
+};
--- api/appuser/services/Appuser.js
+++ api/appuser/services/Appuser.js
@@ -1,8 +1,21 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils')
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/guides/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  formatAppuser:  appuser => {
+    const user = sanitizeEntity(appuser, { model: strapi.models.appuser })
+
+    let script = user.script
+    script = script ? { state: script.state } : script
+
+    return {
+      ...user,
+      script
+    }
+  }
+};
